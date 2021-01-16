91822df642b744155220b824eb48ae4b7855f471
Nicolle <nvalladares@dynasoft.hn>
2021-01-16 09:38:37 -0600

    Implementing bootstrap and creating new components

---
+++ src/components/MenuItem/MenuItem.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="card card-item">
+        <img class="card-img-top" src="https://i.postimg.cc/mr34nFhd/alireza-etemadi-9-G-o-JBKwi1c-unsplash.jpg" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">La imposible</h5>
+            <p class="card-text">1\/2 Libra de carne envuelta en toc\u00edno, con cebolla morada, queso cheddar, especias...</p>
+        </div>
+        <div class="card-body d-flex justify-content-between w-100">
+            <p class="card-category">Recomendaciones</p>
+            <a class="btn card-price">$91.00</a>
+        </div>
+    </div>
+</template>
+
+<script src="./index.js"></script>
+<style src="./styles.css"></style>
