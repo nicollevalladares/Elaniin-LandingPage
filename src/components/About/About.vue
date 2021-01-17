cc37a4b33bce8a68e2250acccda0ada2666f12b7
Nicolle Valladares <nvalladares@dynasoft.hn>
2021-01-17 11:26:45 -0600

    Container fluid

--- src/components/About/About.vue
+++ src/components/About/About.vue
@@ -1,18 +1,20 @@
 <template>
   <section class="pt-5">
-    <div class="row align-items-center">
-      <div class="about-section col-xs-12 col-md-12 col-lg-6">
-        <img src="@/assets/AboutAssets/About.png"  alt="Snow" style="width:100%;"/>
-        <div class="bottom-right">
-          <span class="about-text">LA COMIDA ES </span>
-          <span class="about-text yellow">NUESTRO ARTE </span>
+    <div class="container-fluid">
+      <div class="row align-items-center">
+        <div class="about-section col-xs-12 col-md-12 col-lg-6">
+          <img src="@/assets/AboutAssets/About.png"  alt="Snow" style="width:100%;"/>
+          <div class="bottom-right">
+            <span class="about-text">LA COMIDA ES </span>
+            <span class="about-text yellow">NUESTRO ARTE </span>
+          </div>
         </div>
-      </div>
-      <div class="about-background col-xs-12 col-md-12 col-lg-6 flex-1">
-        <span class="foodies-text">¿Quien es Foodies?</span>
-        <p class="about-description">
-          Elit irure ad nulla id elit laborum nostrud mollit irure. Velit reprehenderit sunt nulla enim aliquip duis tempor est culpa fugiat consequat culpa consectetur Lorem. Reprehenderit dolore culpa irure eiusmod minim occaecat et id minim ullamco.</p>
-        <a class="link-text" href="/">Contáctanos <ArrowRight /> </a>
+        <div class="about-background col-xs-12 col-md-12 col-lg-6 flex-1">
+          <span class="foodies-text">¿Quien es Foodies?</span>
+          <p class="about-description">
+            Elit irure ad nulla id elit laborum nostrud mollit irure. Velit reprehenderit sunt nulla enim aliquip duis tempor est culpa fugiat consequat culpa consectetur Lorem. Reprehenderit dolore culpa irure eiusmod minim occaecat et id minim ullamco.</p>
+          <a class="link-text" href="/">Contáctanos <ArrowRight /> </a>
+        </div>
       </div>
     </div>
   </section>
