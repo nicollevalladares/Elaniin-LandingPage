39e5db721721dc00807f72d4f6c4b721a223666b
Nicolle <nvalladares@dynasoft.hn>
2021-01-17 11:23:30 -0600

    Adding location section

--- src/components/Location/Location.vue
+++ src/components/Location/Location.vue
@@ -1,5 +1,16 @@
 <template>
-
+    <section class="pt-5">
+       <div class="container-fluid">
+            <div class="row align-items-center">
+            <div class="col-xs-12 col-md-6 col-lg-6 order-md-first">
+                <span>Estamos para ti</span>
+            </div>
+            <div class="col-md-6 col-lg-6 position-relative" align=right>
+                
+            </div>
+        </div>
+       </div>
+    </section>
 </template>
 
 <script src="./index.js"></script>
