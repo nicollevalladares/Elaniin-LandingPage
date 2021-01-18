86f06c0e4cfd2afd86a825dad4422014656353da
Nicolle Valladares <nvalladares@dynasoft.hn>
2021-01-17 18:51:23 -0600

    About responsive

--- src/components/About/About.vue
+++ src/components/About/About.vue
@@ -2,18 +2,19 @@
   <section class="pt-5">
     <div class="container-fluid">
       <div class="row">
-        <div class="about-section col-12 col-lg-6">
-          <img class="w-100" src="@/assets/AboutAssets/About.png"  alt="Snow" />
-          <div class="position-absolute">
-            <span class="text-uppercase">La comida es </span>
-            <span class="text-uppercase">Nuesrto arte </span>
+        <div class="col-12 col-lg-6 px-0">
+          <img class="cover-photo" src="@/assets/AboutAssets/About.png"  alt="Snow" />
+          <div class="position-absolute bottom-right about-title">
+            <span class="text-uppercase">La comida es <strong class="yellow">Nuestro arte</strong></span>
           </div>
         </div>
-        <div class="col-12 col-lg-6">
-          <!-- <span class="foodies-text">¿Quien es Foodies?</span>
-          <p class="about-description">
-            Elit irure ad nulla id elit laborum nostrud mollit irure. Velit reprehenderit sunt nulla enim aliquip duis tempor est culpa fugiat consequat culpa consectetur Lorem. Reprehenderit dolore culpa irure eiusmod minim occaecat et id minim ullamco.</p>
-          <a class="link-text" href="/">Contáctanos <ArrowRight /> </a> -->
+        <div class="col-12 col-lg-6 d-flex align-items-center about-background px-5 py-80">
+          <div>
+            <h1 class="foodies-text">¿Quien es Foodies?</h1>
+            <p class="about-description">
+              Elit irure ad nulla id elit laborum nostrud mollit irure. Velit reprehenderit sunt nulla enim aliquip duis tempor est culpa fugiat consequat culpa consectetur Lorem. Reprehenderit dolore culpa irure eiusmod minim occaecat et id minim ullamco.</p>
+            <a class="link-text" href="/">Contáctanos <ArrowRight /> </a>
+          </div>
         </div>
       </div>
     </div>
